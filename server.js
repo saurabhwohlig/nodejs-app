a87a78b191edfd830616e31f04aa4e250390f2c7
Saurabh Mishra <saurabh.mishra@wohlig.com>
2023-12-20 15:11:20 +0530

    changes

--- server.js
+++ server.js
@@ -73,6 +73,6 @@ app.get("/rest/getAllUsers", (req, res) => {
     res.send(userData)
    });
 
-app.listen(PORT, () => {
-  console.log("Server runningg");
-});
+   app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
+  });
